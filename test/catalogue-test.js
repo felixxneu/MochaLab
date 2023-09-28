7daf3138bac82bd0f988fbcf85cd87e2d5c954b7
Felix Neumann <felixneumann71@gmail.com>
2023-09-28 12:11:32 +0100

    Tested remove and find a product by id methods.

--- test/catalogue-test.js
+++ test/catalogue-test.js
@@ -2,17 +2,18 @@ const chai = require("chai");
 const Catalogue = require("../src/productCatalogue");
 const Product = require("../src/product");
 
-const expect = chai.expect
-let cat = null     // Catalogue object ref
-let batch = null   // For later
-
-// Setup
-cat = new Catalogue("Test Catalogue");
-cat.addProduct(new Product("A123", "Product 1", 100, 10, 10.0));
-cat.addProduct(new Product("A124", "Product 2", 100, 10.0));
-cat.addProduct(new Product("A125", "Product 3", 100, 10, 10.0));
+const expect = chai.expect;
+let cat = null;
+let batch = null;
 
 describe("Catalogue", () => {
+  beforeEach( () => {
+    cat = new Catalogue("Test Catalogue");
+    cat.addProduct(new Product("A123", "Product 1", 100, 10, 10.0));
+    cat.addProduct(new Product("A124", "Product 2", 100, 10.0));
+    cat.addProduct(new Product("A125", "Product 3", 100, 10, 10.0));
+    console.log('before block');
+  });
   describe("findProductById", function () {
     it("should find a valid product id", function () {
       const result = cat.findProductById("A123");
@@ -20,6 +21,19 @@ describe("Catalogue", () => {
     });
     it("should return undefined for invalid product id", function () {
       const result = cat.findProductById("A321");
+      expect(result).to.be.undefined;
+    });
+  });
+  describe("removeProductById", () => {
+    it("should remove product with a valid id", () => {
+      let result = cat.removeProductById("A123");
+      expect(result.id).to.equal("A123");
+      // Check target state
+      result = cat.findProductById("A123");
+      expect(result).to.be.undefined;
+    });
+    it("should return undefined when product id is invalid.", () => {
+      const result = cat.removeProductById("A321");
       expect(result).to.be.undefined;
     });
   });
