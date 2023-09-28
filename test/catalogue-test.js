ca9c226978b6e3619e4bd357248ef9b1e8e31a74
Felix Neumann <felixneumann71@gmail.com>
2023-09-28 11:49:20 +0100

    Initial structure.

---
+++ test/catalogue-test.js
@@ -0,0 +1,26 @@
+const chai = require("chai");
+const Catalogue = require("../src/productCatalogue");
+const Product = require("../src/product");
+
+const expect = chai.expect
+let cat = null     // Catalogue object ref
+let batch = null   // For later
+
+// Setup
+cat = new Catalogue("Test Catalogue");
+cat.addProduct(new Product("A123", "Product 1", 100, 10, 10.0));
+cat.addProduct(new Product("A124", "Product 2", 100, 10.0));
+cat.addProduct(new Product("A125", "Product 3", 100, 10, 10.0));
+
+describe("Catalogue", () => {
+  describe("findProductById", function () {
+    it("should find a valid product id", function () {
+      const result = cat.findProductById("A123");
+      expect(result.name).to.equal("Product 1");
+    });
+    it("should return undefined for invalid product id", function () {
+      const result = cat.findProductById("A321");
+      expect(result).to.be.undefined;
+    });
+  });
+});
